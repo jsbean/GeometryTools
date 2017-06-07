146080ec2ff65aa5a2240fcb5a4bc85b61e31b02
James <jbean46@gmail.com>
2017-06-07 21:22:31 +0200

    Add explicit conformance to CollisionDetectable

--- GeometryTools/PolygonProtocol.swift
+++ GeometryTools/PolygonProtocol.swift
@@ -10,7 +10,7 @@ import Collections
 import ArithmeticTools
 
 /// Interface for polygonal shapes.
-public protocol PolygonProtocol: Shape {
+public protocol PolygonProtocol: Shape, CollisionDetectable {
     
     // MARK: - Instance Properties
     
