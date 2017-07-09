0ee9cbf6e800aaa26a53079eb4c1c7c574bc765a
Brian Heim <brianlheim@gmail.com>
2017-07-09 11:20:54 -0400

    Add a stub test for normalized

--- GeometryToolsTests/RectangleTests.swift
+++ GeometryToolsTests/RectangleTests.swift
@@ -6,3 +6,15 @@
 //
 //
 
+
+import XCTest
+import GeometryTools
+
+class RectangleTests {
+
+    func testNormalized() {
+        let r = Rectangle(origin: Point(x: 0, y: 5), size: Size(width: -1, height: -2))
+        let expected = Rectangle(origin: Point(x: -1, y: 3), size: Size(width: 1, height: 2))
+        XCTAssertEqual(r, expected)
+    }
+}
